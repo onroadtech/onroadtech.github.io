68c413a1f356a3c7f440588d8e592adf9b2068f3
onroadtech <onroad_tech@163.com>
2018-02-05 14:32:58 +0800

    Create new website style base on website-template v1.0.0.

---
+++ README.md
@@ -0,0 +1,9 @@
+It is my personal blog base on [Minimal Mistakes Jekyll Theme](https://mmistakes.github.io/minimal-mistakes/) [V4.9.0](https://github.com/mmistakes/minimal-mistakes/releases/tag/4.9.0).
+
+
+
+### License
+
+The license of [Minimal Mistakes Jekyll Theme](https://mmistakes.github.io/minimal-mistakes/) is MIT License, and Copyright (c) 2013-2018 Michael Rose and contributors.
+
+The layout/style of the website and articles are under MIT License too, and Copyright (c) 2017-2018 Liting Wang/王理停.
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Article One",
+        "excerpt":"Content of article one page.1. Title OneContent of title one!Content of title one!Content of title one!Content of title one!1.1 Subtitle oneContent of subtitle one!Content of subtitle one!1.2 Subtitle twoContent of subtitle two!Content of subtitle two!2. Title TwoContent of title two!Content of title two!Content of title two!Content of title two!","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/topic-one-article-one/",
+        "teaser":null},{
+        "title": "Article Two",
+        "excerpt":"Content of article one page.1. Title OneContent of title one!Content of title one!Content of title one!Content of title one!1.1 Subtitle oneContent of subtitle one!Content of subtitle one!1.2 Subtitle twoContent of subtitle two!Content of subtitle two!2. Title TwoContent of title two!Content of title two!Content of title two!Content of title two!","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/topic-one-article-two/",
+        "teaser":null},{
+        "title": "Topic One Introduce",
+        "excerpt":"It is a page for introducing topic one.Content of introducing topic one.","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/topic-one-introduce/",
+        "teaser":null},{
+        "title": "Article One",
+        "excerpt":"Content of article one page.1. Title OneContent of title one!Content of title one!Content of title one!Content of title one!1.1 Subtitle oneContent of subtitle one!Content of subtitle one!1.2 Subtitle twoContent of subtitle two!Content of subtitle two!2. Title TwoContent of title two!Content of title two!Content of title two!Content of title two!","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/topic-two-article-one/",
+        "teaser":null},{
+        "title": "Article Two",
+        "excerpt":"Content of article two page.1. Title OneContent of title one!Content of title one!Content of title one!Content of title one!1.1 Subtitle oneContent of subtitle one!Content of subtitle one!1.2 Subtitle twoContent of subtitle two!Content of subtitle two!2. Title TwoContent of title two!Content of title two!Content of title two!Content of title two!","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/topic-two-article-two/",
+        "teaser":null},{
+        "title": "Topic Two Introduce",
+        "excerpt":"It is a page for introducing topic two.Content of introducing topic two.","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/topic-two-introduce/",
+        "teaser":null}]
